f7c75e1a9eb470ace635850858e2bda22f067db8
AzzikraPraqastaKusuma123 <azzikrapraqasta1@gmail.com>
2025-04-22 18:43:48 +0700

    perbaikan ukuran size

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -3,37 +3,41 @@ import "./Footer.css";
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="footer-container">
-                <div className="footer-logo">
-                    <img src="/assets/img/LogoKecil.png" alt="Logo Cerdas Hukum" />
-                </div>
-                <div className="footer-content">
-                    <div className="customer-care">
-                        <h3>Layanan Pelanggan</h3>
-                        <p>Whatsapp: +62-851-6564-4356</p>
-                        <p>Instagram: @cerdashukum</p>
-                        <p>Email: cerdashukum@gmail.com</p>
-                        <p><strong>Jam Operasional:</strong><br />Senin-Jumat: 10:00 - 21:00 WIB<br />Sabtu: 10:00 - 17:00 WIB</p>
+        <footer className="footer-wrapper">
+            <div className="footer-background">
+                <div className="footer-container">
+                    <div className="footer-logo">
+                        <img src="/assets/img/LogoKecil.png" alt="Logo Cerdas Hukum" />
                     </div>
-                    <div className="account">
-                        <h3>Akun Saya</h3>
-                        <p><Link to="/">Profil</Link></p>
-                        <p><Link to="/signup">Daftar</Link></p>
-                        <p><Link to="/Login">Masuk</Link></p>
-                    </div>
-                    <div className="social-media">
-                        <h3>Ikuti Kami:</h3>
-                        <div className="social-icons">
-                            <a href="#"><img src="/assets/images/instagram.png" alt="Instagram" /></a>
-                            <a href="#"><img src="/assets/images/twt.png" alt="Twitter" /></a>
-                            <a href="#"><img src="/assets/images/yt.png" alt="YouTube" /></a>
+
+                    <div className="footer-content">
+                        <div className="customer-care">
+                            <h3>Layanan Pelanggan</h3>
+                            <p>Whatsapp: +62-851-6564-4356</p>
+                            <p>Instagram: @cerdashukum</p>
+                            <p>Email: cerdashukum@gmail.com</p>
+                            <p><strong>Jam Operasional:</strong><br />Senin-Jumat: 10:00 - 21:00 WIB<br />Sabtu: 10:00 - 17:00 WIB</p>
+                        </div>
+                        <div className="account">
+                            <h3>Akun Saya</h3>
+                            <p><Link to="/">Profil</Link></p>
+                            <p><Link to="/signup">Daftar</Link></p>
+                            <p><Link to="/login">Masuk</Link></p>
+                        </div>
+                        <div className="social-media">
+                            <h3>Ikuti Kami:</h3>
+                            <div className="social-icons">
+                                <a href="#"><img src="/assets/images/instagram.png" alt="Instagram" /></a>
+                                <a href="#"><img src="/assets/images/twt.png" alt="Twitter" /></a>
+                                <a href="#"><img src="/assets/images/yt.png" alt="YouTube" /></a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
             <div className="footer-bottom">
-                <p>©CERASHUKUM, 2025. SEMUA HAK DILINDUNGI</p>
+                <p>©CERDASHUKUM, 2025. SEMUA HAK DILINDUNGI</p>
             </div>
         </footer>
     );
